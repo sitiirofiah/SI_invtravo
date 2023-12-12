cca605beed7bc5c5127c33dd4e9c5ce5b970b8f3
Siti Rofiah <sitiirofiah46@gmail.com>
2023-12-12 15:45:51 +0700

    Create Query.php

---
+++ Config/Query.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Config;
+
+interface Query
+{
+    public function table($table);
+    public function select($select);
+    public function join($join, $joinOn, $typejoin);
+    public function where($where);
+    public function orderBy($order, $type);
+    public function get();
+    public function result();
+    public function resultArray();
+    public function row();
+    public function rowArray();
+}
+?>
